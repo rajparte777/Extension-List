8eb3981f72fdb0e4d76d271bb0537b29357905ff
project demo <parteraj2005@gmail.com>
2026-02-28 09:19:24 +0530

    push

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const par =document.querySelectorAll('.partitle');
 const headdd = document.querySelector('.headd');
 const boxcontainer = document.querySelectorAll('.box-container');
 
+
+
+
+
+const allButton = document.querySelector('#buton1');
+const activeButton = document.querySelector('#buton2');
+const inactiveButton = document.querySelector('#buton3');
+
+
+
 darkttheme.addEventListener("click", (e) => {
 
     e.preventDefault();
@@ -34,12 +44,72 @@ darkttheme.addEventListener("click", (e) => {
     hed.classList.toggle("head-title");
    });
 
-  
-   
-
    boxcontainer.forEach((box)=> {
     box.classList.toggle("box-border");
 });
+
+});
+
+
+function filterBox(type){
+
+boxcontainer.forEach((box)=>{
+    const toggle = box.querySelector('.toggle');
+
+    if(type ==="all"){
+       box.style.display ="block";
+    }
+   else if(type === "active"){
+        box.style.display =toggle.checked ?"block":"none";
+    }
+    else if(type ==="inactive"){
+                   box.style.display =!toggle.checked ?"block":"none";
+
+    }
+
+})
+
+}
+
+allButton.addEventListener("click",()=>filterBox("all"));
+activeButton.addEventListener("click",()=>filterBox("active"));
+inactiveButton.addEventListener("click",()=>filterBox("inactive"));
+
+
+
+let currentFilter = "all";
+
+allButton.addEventListener("click", ()=>{
+  currentFilter = "all";
+  filterBox(currentFilter);
+});
+
+activeButton.addEventListener("click", ()=>{
+  currentFilter = "active";
+  filterBox(currentFilter);
+});
+
+inactiveButton.addEventListener("click", ()=>{
+  currentFilter = "inactive";
+  filterBox(currentFilter);
+});
+
+boxcontainer.forEach((box)=>{
+  const toggle = box.querySelector('.toggle');
+
+  toggle.addEventListener("change", ()=>{
+    filterBox(currentFilter);
+  });
+});
+
+document.querySelectorAll('.box-container').forEach((box) => {
+
+  const removeButton = box.querySelector('#remove');
+
+  removeButton.addEventListener("click", () => {
+    box.remove();                 
+    filterBox(currentFilter);   
+  });
 
 });
 
